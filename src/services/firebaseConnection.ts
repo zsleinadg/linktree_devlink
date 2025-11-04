b95b08222a82b0e112e0b2b7e6ff530ae30f0139
zsleinadg <danielbrhdgamesfoda@gmail.com>
2025-11-04 12:56:38 -0300

    Finalizando projeto

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCR7_o3tFht24DZteCgn6c9Iu0ulX9IrWE",
+  authDomain: "devlink-479e0.firebaseapp.com",
+  projectId: "devlink-479e0",
+  storageBucket: "devlink-479e0.firebasestorage.app",
+  messagingSenderId: "900963430249",
+  appId: "1:900963430249:web:6867d51dad4db3fb6417f8"
 };
 
 
